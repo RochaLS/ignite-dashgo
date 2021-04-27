d08c6ef08440b4a1797ca03fa301eb3cd4a2d70a
Lucas Rocha <ld.rocha@hotmail.com>
2021-04-26 17:59:46 -0700

    create sign in form using Chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
-export default function Home() {
+import { Flex, Button, Stack, } from '@chakra-ui/react'
+import { Input } from '../components/Form/input'
+
+export default function SignIn() {
   return (
-   <h1>DashGo</h1>
+   <Flex
+    w='100vw'
+    h='100vh'
+    align='center'
+    justify='center'
+   >
+     <Flex
+      as='form'
+      w='100%'
+      maxWidth={360}
+      bg='gray.800'
+      p='8'
+      borderRadius={8}
+      flexDir='column'
+     >
+       <Stack spacing='4'>
+         <Input name='Email' label='E-mail' type='email'/>
+         <Input name='Password' label='Password' type='password'/>
+       </Stack>
+
+       <Button
+        type='submit'
+        mt='6'
+        colorScheme='pink'
+        size='lg'
+       >
+         Sign in
+       </Button>
+
+     </Flex>
+   </Flex>
   )
 }
