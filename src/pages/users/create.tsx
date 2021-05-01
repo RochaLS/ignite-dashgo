a77e931365fc56f6ce18beccd7b677df3b34bb3c
Lucas Rocha <ld.rocha@hotmail.com>
2021-04-30 17:09:48 -0700

    add navigation links

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,23 +1,24 @@
 import { Flex, Box, Heading, Divider, VStack, SimpleGrid, HStack, Button } from '@chakra-ui/react'
 import { SideBar } from '../../components/Sidebar'
 import { Input } from '../../components/Form/input'
+import Link from 'next/link'
 
 export default function CreateUser() {
   return (
-    <Flex width='100%' my='6' maxWidth={1480} mx='auto'>  
+    <Flex width='100%' my='6' maxWidth={1480} mx='auto' px='6'>  
       <SideBar />
-      <Box flex='1' boderRadius={8} bg='gray.800' p='8'>
+      <Box flex='1' boderRadius={8} bg='gray.800' p={['6','8']}>
         <Heading size='lg' fontWeigh='normal'>Create user</Heading>
 
         <Divider my='6' borderColor='gray.700' />
 
         <VStack spacing='8'>
-          <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+          <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
             <Input name='name' label='Full name' />
             <Input name='email' label='E-mail' type='email' />
           </SimpleGrid>
 
-          <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+          <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
             <Input name='password' label='Password' type='password' />
             <Input name='password_confirmation' label='Password confirmation' type='password' />
           </SimpleGrid>
@@ -25,7 +26,9 @@ export default function CreateUser() {
 
         <Flex mt='8' justify='flex-end'>
           <HStack spacing='4'>
-            <Button colorScheme='whiteAlpha'>Cancel</Button>
+            <Link href='/users' passHref>
+              <Button colorScheme='whiteAlpha'>Cancel</Button>
+            </Link>
             <Button colorScheme='pink'>Save</Button>
           </HStack>
         </Flex>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from '@chakra-ui/react'
+import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
@@ -19,14 +20,16 @@ export default function UserList() {
         <Box flex='1' boderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>Users</Heading>
-            <Button
-              as='a' 
-              size='sm' 
-              fontSize='small' 
-              colorScheme='pink' 
-              leftIcon={<Icon as={RiAddLine} fontSize='20' />}>
-              Add new
-            </Button>
+            <Link href='/users/create' passHref>
+              <Button
+                as='a' 
+                size='sm' 
+                fontSize='small' 
+                colorScheme='pink' 
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}>
+                Add new
+              </Button>
+            </Link>
             
           </Flex>
           <Table colorScheme='whiteAlpha'>
