e674d73b3682d9cf78436c0c43f941af37d27701
Lucas Rocha <ld.rocha@hotmail.com>
2021-05-12 16:49:53 -0700

    implement react-query to persist data on the cache

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,20 +1,28 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from '@chakra-ui/react'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { RiAddLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { SideBar } from '../../components/Sidebar'
+import { useQuery } from 'react-query'
 
 export default function UserList() {
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, [])
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
 
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString() 
+      }
+    })
 
+    return users
+  })
 
   const isWideVersion = useBreakpointValue({
     bade: false,
@@ -42,7 +50,17 @@ export default function UserList() {
             </Link>
             
           </Flex>
-          <Table colorScheme='whiteAlpha'>
+         { isLoading ? (
+           <Flex justify='center'>
+             <Spinner />
+           </Flex>
+         ) : error ? (
+           <Flex justify='center'>
+             <Text>Falha ao obter dados!</Text>
+           </Flex>
+         ) : (
+           <>
+            <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
                 <Th px={['4','4','6']} color='gray.300' width='8'>
@@ -53,21 +71,28 @@ export default function UserList() {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td px={['4','4','6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Lucas Rocha</Text>
-                    <Text fontSize='sm' color='gray.300'>ld.rocha@hotmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>27 de Abril, 2021</Td> }
-              </Tr>
+              { data.map(user => {
+                return (
+                  <Tr>
+                    <Td px={['4','4','6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>{user.name}</Text>
+                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>{user.createdAt}</Td> }
+                  </Tr>
+                )
+              })}
             </Tbody>
-          </Table>
-          <Pagination />
+            </Table>
+            <Pagination />
+          </>
+         )
+         }
         </Box>
       </Flex>
     </Box>
